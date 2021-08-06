c8823484d6a89d117069881590d83789f430728f
Ashwin Mahadevan <ashwin.devan01@gmail.com>
2021-08-06 15:54:16 -0400

    Finished 23

--- lab23/DistributionSorts.java
+++ lab23/DistributionSorts.java
@@ -5,7 +5,29 @@ public class DistributionSorts {
     /* Destructively sorts ARR using counting sort. Assumes that ARR contains
        only 0, 1, ..., 9. */
     public static void countingSort(int[] arr) {
-        // TODO: YOUR CODE HERE
+
+        int[] counts = new int[10];
+
+        for (int a : arr) {
+            counts[a] += 1;
+        }
+
+        int[] starts = new int[10];
+
+        starts[0] = 0;
+
+        for (int i = 1; i < 10; i++) {
+            starts[i] = starts[i - 1] + counts[i - 1];
+        }
+
+        int[] output = new int[arr.length];
+
+        for (int a : arr) {
+            output[starts[a]] = a;
+            starts[a] += 1;
+        }
+
+        System.arraycopy(output, 0, arr, 0, arr.length);
     }
 
     /* Destructively sorts ARR using LSD radix sort. */
@@ -20,7 +42,40 @@ public class DistributionSorts {
        DIGIT-th digit. When DIGIT is equal to 0, sort the numbers by the
        rightmost digit of each number. */
     private static void countingSortOnDigit(int[] arr, int digit) {
-        // TODO: YOUR CODE HERE
+
+        int[] counts = new int[10];
+
+        for (int a : arr) {
+            int d = getDigit(a, digit);
+            counts[d] += 1;
+        }
+
+        int[] starts = new int[10];
+        starts[0] = 0;
+
+        for (int i = 1; i < 10; i++) {
+            starts[i] = starts[i - 1] + counts[i - 1];
+        }
+
+        int[] output = new int[arr.length];
+
+        for (int a : arr) {
+            int d = getDigit(a, digit);
+            output[starts[d]] = a;
+            starts[d] += 1;
+        }
+
+        System.arraycopy(output, 0, arr, 0, arr.length);
+    }
+
+    private static int getDigit(int n, int digit) {
+
+        while (digit > 0) {
+            digit -= 1;
+            n /= 10;
+        }
+
+        return n % 10;
     }
 
     /* Returns the largest number of digits that any integer in ARR has. */
@@ -60,7 +115,7 @@ public class DistributionSorts {
     private static void runLSDRadixSort(int len) {
         int[] arr2 = new int[len];
         for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = randomDigit();
+            arr2[i] = randomInt();
         }
         System.out.println("Original array: " + Arrays.toString(arr2));
         lsdRadixSort(arr2);
